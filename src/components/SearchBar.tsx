4c8baa275ebbce9eac5e95d52d7977d45a2e9a6c
Reymark <generaltony663@gmail.com>
2024-12-18 21:07:39 +0800

    help

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+
+const SearchBar = () => {
+  return (
+    <form action="" className="">
+      <input type="text" placeholder="Search"/>
+      <button className="cursor-pointer"></button>
+    </form>
+  );
+};
+
+export default SearchBar;
